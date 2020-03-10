7a03b05a992a575db5c459821f11405ffc32f8f7
Matteo Arru <matteo.arru@it.clara.net>
2020-03-10 14:45:47 +0100

    test if chromedriver works

---
+++ seleniumtest/try_selenium.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 10 09:03:35 2020
+
+@author: matteoarru
+"""
+
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome('./resources/chromedriver')  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
